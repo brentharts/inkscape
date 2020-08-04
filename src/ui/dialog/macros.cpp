ec1119c942615d59f9491a6496bd98eaa4375042
Abhay Raj Singh <abhayonlyone@gmail.com>
2020-08-04 21:07:30 +0530

    Setup done for macros feature - Translations - Added to CMakeLists.txt

---
+++ src/ui/dialog/macros.cpp
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/**
+ * @file
+ * Macros dialog - implementation.
+ * Macros group of action that can be repeated many times
+ */
+/* Author:
+ *   Abhay Raj Singh <abhayonlyone@gmail.com>
+ *
+ * Copyright (C) 2020 Authors
+ * Released under GNU GPL v2+, read the file 'COPYING' for more information.
+ */
+
+#include "macros.h"
---
+++ src/ui/dialog/macros.h
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/**
+ * @file
+ * Macros dialog - implementation.
+ * Macros group of action that can be repeated many times
+ */
+/* Author:
+ *   Abhay Raj Singh <abhayonlyone@gmail.com>
+ *
+ * Copyright (C) 2020 Authors
+ * Released under GNU GPL v2+, read the file 'COPYING' for more information.
+ */
+
+#ifndef INKSCAPE_UI_DIALOG_MACROS_H
+#define INKSCAPE_UI_DIALOG_MACROS_H
+
+#endif // INKSCAPE_UI_DIALOG_MACROS_H
