821b0971eee1b954924d02f6683580000111b2c1
Abhay Raj Singh <abhayonlyone@gmail.com>
2020-08-06 09:47:45 +0530

    Linked UI glade files

--- src/ui/dialog/macros.cpp
+++ src/ui/dialog/macros.cpp
@@ -12,3 +12,98 @@
  */
 
 #include "macros.h"
+
+#include <gtkmm/builder.h>
+#include <iostream>
+#include <sigc++/functors/mem_fun.h>
+
+#include "io/resource.h"
+#include "ui/widget/panel.h"
+
+namespace Inkscape {
+namespace UI {
+namespace Dialog {
+
+Macros::Macros()
+    : UI::Widget::Panel()
+{
+    std::string gladefile = IO::Resource::get_filename_string(Inkscape::IO::Resource::UIS, "dialog-macros.glade");
+    {
+        Glib::RefPtr<Gtk::Builder> builder;
+        try {
+            builder = Gtk::Builder::create_from_file(gladefile);
+        } catch (const Glib::Error &ex) {
+            g_warning("GtkBuilder file loading failed for Macros dialog");
+            return;
+        }
+
+        // Linking UI
+        builder->get_widget("MacroBase", _MacroBase);
+
+        builder->get_widget("MacroCreate", _MacroCreate);
+        builder->get_widget("MacroDelete", _MacroDelete);
+        builder->get_widget("MacroImport", _MacroImport);
+        builder->get_widget("MacroExport", _MacroExport);
+
+        builder->get_widget("MacroRecord", _MacroRecord);
+        builder->get_widget("MacroPlay", _MacroPlay);
+        builder->get_widget("MacroEdit", _MacroEdit);
+
+        // Adding signals
+        _MacroCreate->signal_button_press_event().connect(sigc::mem_fun(*this, &Macros::on_macro_create));
+        _MacroDelete->signal_button_press_event().connect(sigc::mem_fun(*this, &Macros::on_macro_delete));
+        _MacroImport->signal_button_press_event().connect(sigc::mem_fun(*this, &Macros::on_macro_import));
+        _MacroExport->signal_button_press_event().connect(sigc::mem_fun(*this, &Macros::on_macro_export));
+
+        _MacroRecord->signal_button_press_event().connect(sigc::mem_fun(*this, &Macros::on_macro_record));
+        _MacroPlay->signal_button_press_event().connect(sigc::mem_fun(*this, &Macros::on_macro_play));
+        _MacroEdit->signal_button_press_event().connect(sigc::mem_fun(*this, &Macros::on_macro_edit));
+
+        // TODO: Initialize Marcos tree
+    }
+}
+bool Macros::on_macro_create()
+{
+    std::cout << "Macro create not implemented" << std::endl;
+    return true;
+}
+
+bool Macros::on_macro_delete()
+{
+    std::cout << "Macro delete not implemented" << std::endl;
+    return true;
+}
+
+bool Macros::on_macro_import()
+{
+    std::cout << "Macro import not implemented" << std::endl;
+    return true;
+}
+
+bool Macros::on_macro_export()
+{
+    std::cout << "Macro export not implemented" << std::endl;
+    return true;
+}
+
+bool Macros::on_macro_record()
+{
+    std::cout << "Macro record not implemented" << std::endl;
+    return true;
+}
+
+bool Macros::on_macro_play()
+{
+    std::cout << "Macro play not implemented" << std::endl;
+    return true;
+}
+
+bool Macros::on_macro_edit()
+{
+    std::cout << "Macro edit not implemented" << std::endl;
+    return true;
+}
+
+} // namespace Dialog
+} // namespace UI
+} // namespace Inkscape
--- src/ui/dialog/macros.h
+++ src/ui/dialog/macros.h
@@ -14,4 +14,80 @@
 #ifndef INKSCAPE_UI_DIALOG_MACROS_H
 #define INKSCAPE_UI_DIALOG_MACROS_H
 
+#include <gtkmm/image.h>
+
+#include "ui/widget/panel.h"
+
+namespace Inkscape {
+namespace UI {
+namespace Dialog {
+
+class Macros : public UI::Widget::Panel
+{
+public:
+    Macros();
+    ~Macros() override;
+
+    Macros(Macros const &other) = delete;
+    Macros operator=(Macros const &other) = delete;
+
+    /**
+     * Helper function which returns a new instance of the dialog.
+     * getInstance is needed by the dialog manager (Inkscape::UI::Dialog::DialogManager).
+     */
+    static Macros &getInstance() { return *new Macros(); }
+
+private:
+    // Event Handlers
+    /**
+     * creates a new macro in the macro tree and asks name
+     */
+    bool on_macro_create();
+
+    /**
+     * deletes selected macro via a confirmation dialog
+     */
+    bool on_macro_delete();
+
+    /**
+     * Pops a file dialog to load a macro file
+     */
+    bool on_macro_import();
+    /**
+     * Pops a file dialog to export a macro file
+     */
+    bool on_macro_export();
+
+    /**
+     * Record new steps after currently selected operation in macro
+     */
+    bool on_macro_record();
+    /**
+     * Plays all steps of a macro
+     */
+    bool on_macro_play();
+    /**
+     * Edits a step in the macro
+     */
+    bool on_macro_edit();
+
+private:
+    // Variables
+
+    // Widgets
+    Gtk::Image *_MacroCreate;
+    Gtk::Image *_MacroDelete;
+    Gtk::Image *_MacroImport;
+    Gtk::Image *_MacroExport;
+    Gtk::Image *_MacroRecord;
+    Gtk::Image *_MacroPlay;
+    Gtk::Image *_MacroEdit;
+
+    Gtk::Box *_MacroBase;
+};
+
+} // namespace Dialog
+} // namespace UI
+} // namespace Inkscape
+
 #endif // INKSCAPE_UI_DIALOG_MACROS_H
