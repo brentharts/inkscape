a88cfa0533cceb8f449105031f5dec83b3a59a0d
vanntile <vanntile@protonmail.com>
2020-08-19 09:16:04 +0300

    changed container dialog calls from names to codes

--- src/ui/contextmenu.cpp
+++ src/ui/contextmenu.cpp
@@ -537,7 +537,7 @@ void ContextMenu::SelectSameObjectType()
 void ContextMenu::ItemProperties()
 {
     _desktop->selection->set(_item);
-    _desktop->getContainer()->new_dialog("ObjectProperties");
+    _desktop->getContainer()->new_dialog(SP_VERB_DIALOG_ITEM);
 }
 
 void ContextMenu::ItemSelectThis()
@@ -572,7 +572,7 @@ void ContextMenu::ItemCreateLink()
     Inkscape::DocumentUndo::done(object->document, SP_VERB_NONE, _("Create link"));
 
     _desktop->selection->set(SP_ITEM(object));
-    _desktop->getContainer()->new_dialog("ObjectAttributes");
+    _desktop->getContainer()->new_dialog(SP_VERB_DIALOG_ATTR);
 }
 
 void ContextMenu::SetMask()
@@ -656,7 +656,7 @@ void ContextMenu::MakeAnchorMenu()
 
 void ContextMenu::AnchorLinkProperties()
 {
-    _desktop->getContainer()->new_dialog("ObjectAttributes");
+    _desktop->getContainer()->new_dialog(SP_VERB_DIALOG_ATTR);
 }
 
 void ContextMenu::AnchorLinkFollow()
@@ -737,7 +737,7 @@ void ContextMenu::MakeImageMenu ()
 
 void ContextMenu::ImageProperties()
 {
-    _desktop->getContainer()->new_dialog("ObjectAttributes");
+    _desktop->getContainer()->new_dialog(SP_VERB_DIALOG_ATTR);
 }
 
 Glib::ustring ContextMenu::getImageEditorName(bool is_svg) {
@@ -841,7 +841,7 @@ void ContextMenu::ImageEdit()
 
 void ContextMenu::ImageTraceBitmap()
 {
-    _desktop->getContainer()->new_dialog("Trace");
+    _desktop->getContainer()->new_dialog(SP_VERB_SELECTION_TRACE);
 }
 
 void ContextMenu::ImageEmbed()
@@ -891,7 +891,7 @@ void ContextMenu::FillSettings()
         _desktop->selection->set(_item);
     }
 
-    _desktop->getContainer()->new_dialog("FillAndStroke");
+    _desktop->getContainer()->new_dialog(SP_VERB_DIALOG_FILL_STROKE);
 }
 
 void ContextMenu::MakeTextMenu ()
@@ -925,7 +925,7 @@ void ContextMenu::TextSettings ()
         _desktop->selection->set(_item);
     }
 
-    _desktop->getContainer()->new_dialog("TextFont");
+    _desktop->getContainer()->new_dialog(SP_VERB_DIALOG_TEXT);
 }
 
 void ContextMenu::SpellcheckSettings ()
@@ -935,7 +935,7 @@ void ContextMenu::SpellcheckSettings ()
         _desktop->selection->set(_item);
     }
 
-    _desktop->getContainer()->new_dialog("SpellCheck");
+    _desktop->getContainer()->new_dialog(SP_VERB_DIALOG_SPELLCHECK);
 #endif
 }
 
