a41d5d9580e79b71e5e6743cbf06f8d7f14e63a9
Martin Owens <doctormo@gmail.com>
2021-02-14 16:16:58 -0500

    Revert previous commit and calculate filters in geometric space

--- src/object/sp-filter.cpp
+++ src/object/sp-filter.cpp
@@ -340,10 +340,13 @@ Inkscape::XML::Node* SPFilter::write(Inkscape::XML::Document *doc, Inkscape::XML
 
 /**
  * Update the filter's region based on it's detectable href links
+ *
+ * Automatic region only updated if auto_region is false
+ * and filterUnits is not UserSpaceOnUse
  */
 void SPFilter::update_filter_all_regions()
 {
-    if (!this->auto_region)
+    if (!this->auto_region || this->filterUnits == SP_FILTER_UNITS_USERSPACEONUSE)
         return;
 
     // Combine all items into one region for updating.
@@ -365,7 +368,7 @@ void SPFilter::update_filter_all_regions()
  */
 void SPFilter::update_filter_region(SPItem *item)
 {
-    if (!this->auto_region)
+    if (!this->auto_region || this->filterUnits == SP_FILTER_UNITS_USERSPACEONUSE)
         return; // No adjustment for dead box
 
     auto region = this->get_automatic_filter_region(item);
@@ -381,11 +384,17 @@ void SPFilter::update_filter_region(SPItem *item)
  */
 Geom::Rect SPFilter::get_automatic_filter_region(SPItem *item)
 {
-    Geom::OptRect bbox = item->bbox(Geom::identity(), SPItem::VISUAL_BBOX);
-    if (!bbox) return Geom::Rect(); // No adjustment for dead box
+    // Calling bbox instead of visualBound() avoids re-requesting filter regions
+    Geom::OptRect v_box = item->bbox(Geom::identity(), SPItem::VISUAL_BBOX);
+    Geom::OptRect g_box = item->bbox(Geom::identity(), SPItem::GEOMETRIC_BBOX);
+    if (!v_box || !g_box) return Geom::Rect(); // No adjustment for dead box
 
-    Geom::Rect inbox = *bbox;
-    Geom::Rect outbox = inbox;
+    // Because the filter box is in geometric bounding box units, it must ALSO
+    // take account of the visualBox, so even if the filter does NOTHING to the
+    // size of an object, we must add the difference between the geometric and
+    // visual boxes ourselves or find them cut off by renderers of all kinds.
+    Geom::Rect inbox = *g_box;
+    Geom::Rect outbox = *v_box;
     for(auto& primitive_obj: this->children) {
         auto primitive = dynamic_cast<SPFilterPrimitive *>(&primitive_obj);
         if (primitive) {
