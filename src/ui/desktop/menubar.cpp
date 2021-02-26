5e399643f855f930538f19998036a001bf7d7175
Jabier Arraiza <jabier.arraiza@marker.es>
2021-02-26 18:21:13 +0000

    Try to fix open menu

--- src/ui/desktop/menubar.cpp
+++ src/ui/desktop/menubar.cpp
@@ -640,10 +640,10 @@ reload_menu(Inkscape::UI::View::View* view, Gtk::MenuBar* menubar)
     }
     menuitems.clear();
     build_menu(menubar, INKSCAPE.get_menus()->parent(), view);
+    menubar->show_all();
 #ifdef GDK_WINDOWING_QUARTZ
     sync_menubar();
-#else
-    menubar->show_all();
+    menubar->hide();
 #endif
 }
 
