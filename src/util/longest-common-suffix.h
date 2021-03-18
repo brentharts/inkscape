13c26dbec550913c96e953b48ca3411dd3d82a2f
Apoorv-Ban <heroapoorvbanerjee@gmail.com>
2021-03-18 19:00:17 +0000

    Update longest-common-suffix.h with respect to #1762

--- src/util/longest-common-suffix.h
+++ src/util/longest-common-suffix.h
@@ -83,7 +83,7 @@ ForwardIterator longest_common_suffix(ForwardIterator a, ForwardIterator b,
     ForwardIterator longest_common(end);
 
     while ( !suffixes[0].empty() && !suffixes[1].empty() &&
-            pred(*(suffixes[0].back()), *(suffixes[1].back())) )
+            (*(suffixes[0].back()), *(suffixes[1].back())) )
     {
         longest_common = suffixes[0].back();
         suffixes[0].pop_back();
