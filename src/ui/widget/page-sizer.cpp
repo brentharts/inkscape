585a91d398e844bf4b48b329025b66bbf42e6f4d
LittleStar21 <leonardian14@gmail.com>
2021-03-29 19:50:35 +0800

    Fixes https://gitlab.com/inkscape/inkscape/-/issues/480 Fixed the tooltip text

--- src/ui/widget/page-sizer.cpp
+++ src/ui/widget/page-sizer.cpp
@@ -222,7 +222,8 @@ PageSizer::PageSizer(Registry & _wr)
     Gtk::AccelKey shortcut_key = Inkscape::Shortcuts::getInstance().get_shortcut_from_verb(verb);
     Glib::ustring label_string = Inkscape::Shortcuts::get_label(shortcut_key);
     _fitPageButton.set_label(_(("_Resize page to drawing or selection (" + label_string + ")").c_str()));
-    _fitPageButton.set_tooltip_text(_("Resize the page to fit the current selection, or the entire drawing if there is no selection"));
+    Glib::ustring tooltipText = Glib::ustring::compose(_("_Resize page to drawing or selection (%1)"), label_string);
+    _fitPageButton.set_tooltip_text(tooltipText);
 
     _fitPageButton.set_hexpand();
     _fitPageButton.set_halign(Gtk::ALIGN_CENTER);
