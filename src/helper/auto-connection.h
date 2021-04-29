87f4ce6ab89e71ccb0d83af08d92fe2afbb12e43
mike kowalski <michal_kowalski@hotmail.com>
2021-04-28 20:57:25 -0700

    spacing out

--- src/helper/auto-connection.h
+++ src/helper/auto-connection.h
@@ -11,30 +11,30 @@ namespace Inkscape {
 
 class auto_connection {
 public:
-	auto_connection(const sigc::connection& c): _connection(c) {}
+    auto_connection(const sigc::connection& c): _connection(c) {}
 
-	auto_connection() = default;
+    auto_connection() = default;
 
-	~auto_connection() {
-		_connection.disconnect();
-	}
+    ~auto_connection() {
+        _connection.disconnect();
+    }
 
-	auto_connection(const auto_connection&) = delete;
-	auto_connection& operator = (const auto_connection&) = delete;
+    auto_connection(const auto_connection&) = delete;
+    auto_connection& operator = (const auto_connection&) = delete;
 
-	// re-assign
-	auto_connection& operator = (const sigc::connection& c) {
-		_connection.disconnect();
-		_connection = c;
-		return *this;
-	}
+    // re-assign
+    auto_connection& operator = (const sigc::connection& c) {
+        _connection.disconnect();
+        _connection = c;
+        return *this;
+    }
 
-	void disconnect() {
-		*this = sigc::connection();
-	}
+    void disconnect() {
+        *this = sigc::connection();
+    }
 
 private:
-	sigc::connection _connection;
+    sigc::connection _connection;
 };
 
 } // namespace
--- src/ui/widget/gradient-editor.h
+++ src/ui/widget/gradient-editor.h
@@ -29,80 +29,80 @@ class GradientSelector;
 
 class GradientEditor : public Gtk::Box, public GradientSelectorInterface {
 public:
-	GradientEditor(const char* prefs);
-	~GradientEditor() noexcept override;
+    GradientEditor(const char* prefs);
+    ~GradientEditor() noexcept override;
 
 private:
-	sigc::signal<void> _signal_grabbed;
-	sigc::signal<void> _signal_dragged;
-	sigc::signal<void> _signal_released;
-	sigc::signal<void, SPGradient*> _signal_changed;
+    sigc::signal<void> _signal_grabbed;
+    sigc::signal<void> _signal_dragged;
+    sigc::signal<void> _signal_released;
+    sigc::signal<void, SPGradient*> _signal_changed;
 
 public:
-	decltype(_signal_changed) signal_changed() const { return _signal_changed; }
-	decltype(_signal_grabbed) signal_grabbed() const { return _signal_grabbed; }
-	decltype(_signal_dragged) signal_dragged() const { return _signal_dragged; }
-	decltype(_signal_released) signal_released() const { return _signal_released; }
+    decltype(_signal_changed) signal_changed() const { return _signal_changed; }
+    decltype(_signal_grabbed) signal_grabbed() const { return _signal_grabbed; }
+    decltype(_signal_dragged) signal_dragged() const { return _signal_dragged; }
+    decltype(_signal_released) signal_released() const { return _signal_released; }
 
-	void setGradient(SPGradient* gradient) override;
-	SPGradient* getVector() override;
-	void setVector(SPDocument* doc, SPGradient* vector) override;
-	void setMode(SelectorMode mode) override;
-	void setUnits(SPGradientUnits units) override;
-	SPGradientUnits getUnits() override;
-	void setSpread(SPGradientSpread spread) override;
-	SPGradientSpread getSpread() override;
-	void selectStop(SPStop* selected) override;
+    void setGradient(SPGradient* gradient) override;
+    SPGradient* getVector() override;
+    void setVector(SPDocument* doc, SPGradient* vector) override;
+    void setMode(SelectorMode mode) override;
+    void setUnits(SPGradientUnits units) override;
+    SPGradientUnits getUnits() override;
+    void setSpread(SPGradientSpread spread) override;
+    SPGradientSpread getSpread() override;
+    void selectStop(SPStop* selected) override;
 
 private:
-	void set_gradient(SPGradient* gradient);
-	void stop_selected();
-	void insert_stop_at(double offset);
-	void add_stop(SPStop* current);
-	void duplicate_stop();
-	void delete_stop(int index);
-	void toggle_stops();
-	void update_stops_layout();
-	void set_repeat_mode(SPGradientSpread mode);
-	void set_repeat_icon(SPGradientSpread mode);
-	void reverse_gradient();
-	void set_stop_color(SPColor color, float opacity);
-	std::optional<Gtk::TreeRow> current_stop();
-	SPStop* get_nth_stop(size_t index);
-	SPStop* get_current_stop();
-	void select_stop(size_t index);
-	void set_stop_offset(size_t index, double offset);
-	SPGradient* get_gradient_vector();
-	void fire_stop_selected(SPStop* stop);
+    void set_gradient(SPGradient* gradient);
+    void stop_selected();
+    void insert_stop_at(double offset);
+    void add_stop(SPStop* current);
+    void duplicate_stop();
+    void delete_stop(int index);
+    void toggle_stops();
+    void update_stops_layout();
+    void set_repeat_mode(SPGradientSpread mode);
+    void set_repeat_icon(SPGradientSpread mode);
+    void reverse_gradient();
+    void set_stop_color(SPColor color, float opacity);
+    std::optional<Gtk::TreeRow> current_stop();
+    SPStop* get_nth_stop(size_t index);
+    SPStop* get_current_stop();
+    void select_stop(size_t index);
+    void set_stop_offset(size_t index, double offset);
+    SPGradient* get_gradient_vector();
+    void fire_stop_selected(SPStop* stop);
 
-	Glib::RefPtr<Gtk::Builder> _builder;
-	GradientSelector* _selector;
-	Inkscape::UI::SelectedColor _selected_color;
-	Gtk::Popover& _popover;
-	Gtk::Image& _repeat_icon;
-	GradientWithStops _gradient_image;
-	Glib::RefPtr<Gtk::ListStore> _stop_list_store;
-	Gtk::TreeModelColumnRecord _stop_columns;
-	Gtk::TreeModelColumn<SPStop*> _stopObj;
-	Gtk::TreeModelColumn<size_t> _stopIdx;
-	Gtk::TreeModelColumn<Glib::ustring> _stopID;
-	Gtk::TreeModelColumn<Glib::RefPtr<Gdk::Pixbuf>> _stop_color;
-	Gtk::TreeView& _stop_tree;
-	Gtk::SpinButton& _offset_btn;
-	Gtk::Button& _add_stop;
-	Gtk::Button& _delete_stop;
-	Gtk::Button& _show_stops_list;
-	bool _stops_list_visible = true;
-	Gtk::Box& _stops_gallery;
-	Gtk::Box& _colors_box;
-	Gtk::ToggleButton& _linear_btn;
-	Gtk::ToggleButton& _radial_btn;
-	Gtk::Grid& _main_grid;
-	SPGradient* _gradient = nullptr;
-	SPDocument* _document = nullptr;
-	OperationBlocker _update;
-	OperationBlocker _notification;
-	Glib::ustring _prefs;
+    Glib::RefPtr<Gtk::Builder> _builder;
+    GradientSelector* _selector;
+    Inkscape::UI::SelectedColor _selected_color;
+    Gtk::Popover& _popover;
+    Gtk::Image& _repeat_icon;
+    GradientWithStops _gradient_image;
+    Glib::RefPtr<Gtk::ListStore> _stop_list_store;
+    Gtk::TreeModelColumnRecord _stop_columns;
+    Gtk::TreeModelColumn<SPStop*> _stopObj;
+    Gtk::TreeModelColumn<size_t> _stopIdx;
+    Gtk::TreeModelColumn<Glib::ustring> _stopID;
+    Gtk::TreeModelColumn<Glib::RefPtr<Gdk::Pixbuf>> _stop_color;
+    Gtk::TreeView& _stop_tree;
+    Gtk::SpinButton& _offset_btn;
+    Gtk::Button& _add_stop;
+    Gtk::Button& _delete_stop;
+    Gtk::Button& _show_stops_list;
+    bool _stops_list_visible = true;
+    Gtk::Box& _stops_gallery;
+    Gtk::Box& _colors_box;
+    Gtk::ToggleButton& _linear_btn;
+    Gtk::ToggleButton& _radial_btn;
+    Gtk::Grid& _main_grid;
+    SPGradient* _gradient = nullptr;
+    SPDocument* _document = nullptr;
+    OperationBlocker _update;
+    OperationBlocker _notification;
+    Glib::ustring _prefs;
 };
 
 
