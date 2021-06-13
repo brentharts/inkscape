177dd161f4b7d6b1774cfb5d34378227359f4d40
mike kowalski <michal_kowalski@hotmail.com>
2021-06-13 16:58:09 -0700

    addad license

--- src/ui/builder-utils.cpp
+++ src/ui/builder-utils.cpp
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 #include "builder-utils.h"
 #include "io/resource.h"
 
--- src/ui/widget/color-palette.cpp
+++ src/ui/widget/color-palette.cpp
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 #include <gtkmm/box.h>
 #include <gtkmm/popover.h>
 #include <gtkmm/scale.h>
