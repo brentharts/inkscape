328b8255a6cc8960852606efc76359798f68c82e
mike kowalski <michal_kowalski@hotmail.com>
2021-06-13 16:20:28 -0700

    adding color pallette glade to translations + license

--- src/ui/builder-utils.cpp
+++ src/ui/builder-utils.cpp
@@ -5,15 +5,15 @@ namespace Inkscape {
 namespace UI {
 
 Glib::RefPtr<Gtk::Builder> create_builder(const char* filename) {
-	auto glade = Inkscape::IO::Resource::get_filename(Inkscape::IO::Resource::UIS, filename);
-	Glib::RefPtr<Gtk::Builder> builder;
-	try {
-		return Gtk::Builder::create_from_file(glade);
-	}
-	catch (Glib::Error& ex) {
-		g_error("Cannot load glade file: %s", ex.what().c_str());
-		throw;
-	}
+    auto glade = Inkscape::IO::Resource::get_filename(Inkscape::IO::Resource::UIS, filename);
+    Glib::RefPtr<Gtk::Builder> builder;
+    try {
+        return Gtk::Builder::create_from_file(glade);
+    }
+    catch (Glib::Error& ex) {
+        g_error("Cannot load glade file: %s", ex.what().c_str());
+        throw;
+    }
 }
 
 }} // namespace
