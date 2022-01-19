5423f918acf190d505b8e439173ec918280ebe8f
Ron Burk <rs@rs-math.net>
2022-01-18 23:20:13 -0300

    Fix #3113 - Nonmonotonic Pencil pressure response
    When used with a drawing tablet, the Pencil tool
    responded nonlinearly to the applied pressure, which was certainly not
    the desired behavior.
    
    This commit restores a monotonic, linear dependence of
    the pencil width on the pressure reported by the pointing device.
    
    Fixes https://gitlab.com/inkscape/inkscape/-/issues/3113

--- src/ui/tools/pencil-tool.cpp
+++ src/ui/tools/pencil-tool.cpp
@@ -840,8 +840,8 @@ void PencilTool::_addFreehandPoint(Geom::Point const &p, guint /*state*/, bool l
             min = max;
         }
         double dezoomify_factor = 0.05 * 1000 / desktop->current_zoom();
-        double pressure_shrunk = (((this->pressure - 0.25) * 1.25) * (max - min)) + min;
-        double pressure_computed = std::abs(pressure_shrunk * dezoomify_factor);
+        double pressure_shrunk = this->pressure * (max - min) + min; // C++20 -> use std::lerp()
+        double pressure_computed = pressure_shrunk * dezoomify_factor;
         double pressure_computed_scaled = std::abs(pressure_computed * desktop->getDocument()->getDocumentScale().inverse()[Geom::X]);
         if (p != this->p[this->_npoints - 1]) {
             this->_wps.emplace_back(distance, pressure_computed_scaled);
