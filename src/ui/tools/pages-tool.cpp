7a853517393697a89fd49b1fa05e37aa9483af71
karan malik <7karanmalik@gmail.com>
2024-03-21 15:02:04 +0530

    Added right click context menu for page tool

--- src/ui/tools/pages-tool.cpp
+++ src/ui/tools/pages-tool.cpp
@@ -285,8 +285,8 @@ bool PagesTool::root_handler(CanvasEvent const &event)
                 }
             } else if (event.button == 3) {
 	   	 menu_popup(event, page_manager.getSelected());
-		 ret = true;
-	    }
+                ret = true;
+            }
         },
         [&] (MotionEvent const &event) {
             auto point_w = event.pos;
