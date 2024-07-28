ef1db5fe626159223895ab8e0010c43544a4064e
Kaixo Gamorra <21697330-kaixoo@users.noreply.gitlab.com>
2024-07-28 21:15:04 +0200

    Add styling preferences to debug.cpp

--- src/ui/dialog/debug.cpp
+++ src/ui/dialog/debug.cpp
@@ -29,3 +29,14 @@ Debug::Debug()
 Debug::~Debug() {}
 
 } // namespace Inkscape::UI::Dialog
+
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0)(case-label . +))
+  indent-tabs-mode:nil
+  fill-column:99
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:fileencoding=utf-8:textwidth=99 :
