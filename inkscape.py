4945eb035bb7c1ac5e119918852c45adf36b24c2
brent hartshorn <brenthartshorn@proton.me>
2024-09-11 14:04:13 -0700

    use forks of libcroco and lib2geom

--- inkscape.py
+++ inkscape.py
@@ -12,6 +12,16 @@ _buildir = os.path.join(_thisdir, 'build')
 if not os.path.isdir(_buildir):
 	os.mkdir(_buildir)
 
+_3rdparty = os.path.join(_thisdir, 'src/3rdparty')
+if not os.path.isdir(os.path.join(_3rdparty, 'inkscape_libcroco')):
+	cmd = 'git clone --depth 1 https://github.com/brentharts/inkscape_libcroco.git'
+	print(cmd)
+	subprocess.check_call(cmd.split(), cwd=_3rdparty)
+if not os.path.isdir(os.path.join(_3rdparty, 'inkscape_lib2geom')):
+	cmd = 'git clone --depth 1 https://github.com/brentharts/inkscape_lib2geom.git'
+	print(cmd)
+	subprocess.check_call(cmd.split(), cwd=_3rdparty)
+
 cmd = ['cmake', os.path.abspath(_thisdir)]
 print(cmd)
 subprocess.check_call(cmd, cwd=_buildir)
